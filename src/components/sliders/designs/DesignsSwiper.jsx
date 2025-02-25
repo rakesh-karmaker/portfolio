329f06b0ecd3a82c0456eae7bae48b27cca4f718
rakesh-karmaker <rakeshkarmaker0175@gmail.com>
2025-02-26 00:48:39 +0600

    make the designsSlider more responsive

--- src/components/sliders/designs/DesignsSwiper.jsx
+++ src/components/sliders/designs/DesignsSwiper.jsx
@@ -14,8 +14,6 @@ const DesignsSwiper = ({ designSliderRef, setIndex, index }) => {
       : (90 * window.innerWidth) / 100;
 
   const offset = (window.innerWidth - maxWidth) / 2;
-  const willCenter = index === 0 ? false : true;
-  const willOffset = index === 0 ? offset : 0;
   return (
     <Swiper
       onSwiper={(swiper) => {
@@ -25,28 +23,20 @@ const DesignsSwiper = ({ designSliderRef, setIndex, index }) => {
         setIndex(swiper.realIndex);
       }}
       slidesPerView={"auto"}
-      spaceBetween={50}
+      spaceBetween={window.innerWidth >= 1600 ? 100 : 50}
       className="designs-swiper"
-      centeredSlides={true}
-      breakpoints={{
-        1600: {
-          spaceBetween: 100,
-          centeredSlides: willCenter,
-          slidesOffsetBefore: willOffset,
-        },
-        1250: {
-          spaceBetween: 50,
-          slidesOffsetBefore: willOffset,
-          slidesOffsetAfter: 0,
-          centeredSlides: willCenter,
-          allowTouchMove: false,
-        },
-        950: {
-          slidesOffsetBefore: offset,
-          slidesOffsetAfter: 50,
-          centeredSlides: false,
-        },
-      }}
+      slidesOffsetBefore={
+        window.innerWidth > 950 && window.innerWidth < 1250
+          ? 50
+          : index === 0
+          ? offset
+          : 0
+      }
+      centeredSlides={index === 0 && window.innerWidth > 950 ? false : true}
+      allowTouchMove={window.innerWidth > 1250 ? false : true}
+      slidesOffsetAfter={
+        window.innerWidth > 950 && window.innerWidth < 1250 ? 50 : 0
+      }
     >
       {designList.map((design) => (
         <SwiperSlide key={design.title} className="design-slide">
